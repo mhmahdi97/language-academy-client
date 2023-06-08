3653f338ec74fe811b281d38d6955d7d4ed55a03
mhmahdi97 <mhmahdi97@gmail.com>
2023-06-08 22:54:38 +0600

    created a 404 page with route

--- src/Pages/404/ErrorPage.jsx
+++ src/Pages/404/ErrorPage.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import erroImage from '../../assets/404.png';
+import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
     return (
-        <div>
-            
+       <div className='flex justify-center content-center'>
+             <div >
+                
+                <div>
+                    <img src={erroImage} alt="" />
+                </div>
+                <div className='flex justify-center'>
+                    <Link to='/'>
+                        <button className='bg-slate-300 font-medium px-2 py-1 rounded-lg'> Back to Home</button>
+                    </Link>
+                </div>
+
+            </div>
         </div>
     );
 };
