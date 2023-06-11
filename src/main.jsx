ab04611f544c21778a4978cb6ebb75bce08a69a3
mhmahdi97 <mhmahdi97@gmail.com>
2023-06-12 03:06:28 +0600

    created useAuth hook

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import AuthProvider from './providers/AuthProvider'
-import { RouterProvider } from 'react-router-dom'
-import router from './Routes/Routes'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import AuthProvider from './providers/AuthProvider';
+import { RouterProvider } from 'react-router-dom';
+import router from './Routes/Routes';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
    <div className='max-w-7xl mx-auto'>
   <React.StrictMode>
-    <AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
         <RouterProvider router={router} />
-    </AuthProvider>
+      </AuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
   </div>
 )
