05586c86bb0b1e9f04fe8f73aef2e968934b26b6
mhmahdi97 <mhmahdi97@gmail.com>
2023-06-12 04:15:40 +0600

    finished handle select for selecting course

--- src/Pages/AllCourses/AllCoursesCard.jsx
+++ src/Pages/AllCourses/AllCoursesCard.jsx
@@ -1,17 +1,58 @@
 import React, { useState } from 'react';
+import useAuth from '../../hooks/useAuth';
+import Swal from 'sweetalert2';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const AllCoursesCard = ({course}) => {
 
     const {name, slogan, image, instructorName, availableSeats, price, _id} = course;
     
-    const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+    // const [isButtonDisabled, setIsButtonDisabled] = useState(false);
 
-     const handleSelect = (_id)=> {
+    const {user} = useAuth();
 
+    const navigate = useNavigate();
+    const location = useLocation();
+
+     const handleSelect = (course)=> {
+        if(user && user.email){
+            const selectedCourse = {selectedCourseId: _id, name, slogan, image, price, email: user.email}
+            fetch('http://localhost:5000/selected-courses', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(selectedCourse)
+            })
+            .then(res => res.json())
+            .then(data => {
+                if(data.insertedId){
+                    // refetch(); // refetch cart to update the number of items in the cart
+                    Swal.fire({
+                        position: 'top',
+                        icon: 'success',
+                        title: `The ${slogan} Course Has Been Selected!`,
+                        showConfirmButton: false,
+                        timer: 1500
+                      })
+                }
+            })
+        }
+        else{
+            Swal.fire({
+                title: 'Please Login to Select the Course!',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Login now!'
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  navigate('/login', {state: {from: location}})
+                }
+              })
+        }
      }
-
-
-
 
 
     return (
@@ -23,7 +64,7 @@ const AllCoursesCard = ({course}) => {
             <p className="leading-relaxed text-base mb-3"> <span className='font-medium'>Instructor Name:</span> {instructorName}</p>
             <p className="leading-relaxed text-base mb-3"> <span className='font-medium'>Available Seats:</span> {availableSeats}</p>
             <p className="leading-relaxed text-base mb-7"> <span className='font-medium'>Price:</span>  ${price}</p>
-            <button disabled={availableSeats === 0 ? true : false} onClick={()=>handleSelect(_id)} className={`${availableSeats === 0 ? 'bg-gray-300 cursor-not-allowed' : 'bg-red-500 hover:bg-red-600'} text-white border-0 py-2 px-5 focus:outline-none  rounded text-lg`}>Select Course</button>
+            <button disabled={availableSeats === 0 ? true : false} onClick={()=>handleSelect(course)} className={`${availableSeats === 0 ? 'bg-gray-300 cursor-not-allowed' : 'bg-red-500 hover:bg-red-600'} text-white border-0 py-2 px-5 focus:outline-none  rounded text-lg`}>Select Course</button>
             </div>
         </div>
     );
