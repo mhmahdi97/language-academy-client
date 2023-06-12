658ea010b50d389c2eea766a34ee866e16a68900
mhmahdi97 <mhmahdi97@gmail.com>
2023-06-13 00:51:11 +0600

    implemented dark/light theme toggle mode

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,10 +3,12 @@ import TopSlider from './TopSlider';
 import PopularCourses from './PopularCourses';
 import PopularInstructors from './PopularInstructors';
 import ContactUs from './ContactUs';
+import ToggleDarkLight from './ToggleDarkLight';
 
 const Home = () => {
     return (
         <div>
+            <ToggleDarkLight></ToggleDarkLight>
             <TopSlider></TopSlider>
             <PopularCourses></PopularCourses>
             <PopularInstructors></PopularInstructors>
