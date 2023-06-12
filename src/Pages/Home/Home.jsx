02a1e1f2a9bd1fdfa3ef1169532dbfa5dd025aeb
mhmahdi97 <mhmahdi97@gmail.com>
2023-06-13 01:20:36 +0600

    implement react awsome animation to the home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,15 +4,18 @@ import PopularCourses from './PopularCourses';
 import PopularInstructors from './PopularInstructors';
 import ContactUs from './ContactUs';
 import ToggleDarkLight from './ToggleDarkLight';
+import { Slide } from "react-awesome-reveal";
 
 const Home = () => {
     return (
         <div>
-            <ToggleDarkLight></ToggleDarkLight>
-            <TopSlider></TopSlider>
-            <PopularCourses></PopularCourses>
-            <PopularInstructors></PopularInstructors>
-            <ContactUs></ContactUs>
+            <Slide>
+                <ToggleDarkLight></ToggleDarkLight>
+                <TopSlider></TopSlider>
+                <PopularCourses></PopularCourses>
+                <PopularInstructors></PopularInstructors>
+                <ContactUs></ContactUs>
+            </Slide>
         </div>
     );
 };
