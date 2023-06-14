56599138ce15af21f8d61c9292dcec24fe477de8
mhmahdi97 <mhmahdi97@gmail.com>
2023-06-14 11:49:25 +0600

    created isAdmin hook

--- src/Layout/DashBoard.jsx
+++ src/Layout/DashBoard.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import useAdmin from '../hooks/useAdmin';
 
 const DashBoard = () => {
+
+    const [isAdmin] = useAdmin();
+    console.log(isAdmin)
+
     return (
         <div>
             <Outlet></Outlet>
